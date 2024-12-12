1fe499d5c1e7b992967676a8312f701395172d20
Anakin Skywalker <anakinskiwalker083@gmail.com>
2024-12-12 00:02:52 -0300

    Revert "Update index.php"
    This reverts commit 930e21570833d06e9532babc195731e6f5320fa7.

--- index.php
+++ index.php
@@ -170,8 +170,6 @@ $conn->close();
         <div class="editar_usuario">
             <a href="nuevos_cambios/editar_usuario.php" style="text-decoration: underline;">Editar datos personales</a>
         </div>
-
-        <th>quiero probar github</th>
     </main>
 </body>
 </html>
